3ad2cb522b28686ae1f49923f2e8169a2dfa3252
Sterling Trafford <Sterling_Trafford1@baylor.edu>
2019-12-08 14:06:24 -0600

    WEB: added input page barebones and WTF Forms

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
+from wtforms import Form,StringField,TextAreaField,validators
 
 app = Flask(__name__)
 app.debug= True
@@ -9,11 +10,13 @@ app.debug= True
 def homePage():
         return render_template('index.html')
 
-@app.route("/about")
+@app.route("/input")
 def aboutPage():
-        return render_template("about.html")
+        return render_template("input.html")
 
-
+class AddConditionForm(Form):
+        conditions = StringField('Name', validators=[validators.Length(min=1,max=50)])
+        username = StringField('Username', validators=[validators.Length(min=4,max=25)])
 
 if __name__ == "__main__":
     app.run()
