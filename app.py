90ee715c5aa7e1cbcd6bc680258302c8022ccd6f
Sterling Trafford <Sterling_Trafford1@baylor.edu>
2019-12-08 14:30:22 -0600

    WEB: mysql connector installed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request,redirect,url_for,session, logging
 from wtforms import Form,StringField,TextAreaField,validators
+import mysql.connector
 
 app = Flask(__name__)
 app.debug= True
