764e03b3e67d7d7a355af25751d23b9280afdcc7
Sterling Trafford <Sterling_Trafford1@baylor.edu>
2019-12-10 15:25:22 -0600

    WEB INPUT: validation fix + switch on button type

--- forms.py
+++ forms.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, request,redirect,url_for,session, logging
 from wtforms import Form,StringField,TextAreaField,validators, SelectField, RadioField, ValidationError
 import mysql.connector
+import queries
 
-def validateName(form, field):
-    if(len(field.data) == 0 ):
-        raise ValidationError("Name Already Exists")
-
+#USER INPUT FORMS
 class AddConditionForm(Form):
-        conditions = StringField('Condition Name', [validators.Length(min=6,max=50), validateName])
+        conditions = StringField('Condition Name', [validators.Length(min=1,max=50), validateName])
         domain = RadioField("Domain",choices=[('int',"Integer"),('Boolean',"Boolean"),('String',"String"),("Float","Float")])
 
 class AddMeasurementForm(Form):
-        conditions = StringField('Measurement Name', [validators.Length(min=6,max=50), validateName])
+        conditions = StringField('Measurement Name', [validators.Length(min=1,max=50), validateName])
         domain = RadioField("Domain",choices=[('int',"Integer"),('Boolean',"Boolean"),('String',"String"),("Float","Float")])
 
 class ModifySequenceForm(Form):
-    test = "test"
+    sequence = SelectField("Sequence Name", choices=[queries.getAllSequences])
+    description = StringField("Description",[validators.Length(min=1,max=50), validators.required])
+    filename = StringField("Sequence File", [validators.Length(min=1,max=50)])
 
 class InsertMeasurementForm(Form):
     test = "test"
@@ -23,3 +23,10 @@ class InsertMeasurementForm(Form):
 
 class SequenceSelectForm(Form):
         sequences = SelectField(label="Select Sequence")
+
+
+
+# CUSTOM USER INPUT VALIDATORS
+def validateName(form, field):
+    if(len(field.data) == 0 ):
+        raise ValidationError("Name Already Exists")
--- queries.py
+++ queries.py
@@ -2,7 +2,8 @@
 # file for all database queries/activities 
 
 def getAllSequences():
-    test = 1
+    #need it to return an array of Seq identifier to seqName for SelectField in GUI
+    return [("SequenceIdentifier","SeqName"),("SequenceIdentifier2","SeqName2")]
 
 def getAllConditions():
     test = 1
