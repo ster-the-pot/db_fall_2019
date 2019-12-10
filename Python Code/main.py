7e22abab26f2ded5efd083e50d103ee24da55c66
Baltharaaz <46429788+Baltharaaz@users.noreply.github.com>
2019-12-10 14:32:10 -0600

    INPUT: Finalized first iteration of csv input, requires testing

--- Python Code/main.py
+++ Python Code/main.py
@@ -1,5 +1,6 @@
 import mysql.connector as mysql
 import pandas as pd
+import numpy as np
 
 mydb = mysql.connect(
     host="18.224.202.17",
@@ -9,7 +10,7 @@ mydb = mysql.connect(
 
 cursor = mydb.cursor()
 
-cursor.execute("CREATE DATABASE IF NOT EXISTS GeneExpression")
+cursor.execute("CREATE DATABASE IF NOT EXISTS biology")
 
 cursor.execute("CREATE TABLE IF NOT EXISTS Sequences "
                "(Name varchar(255) PRIMARY KEY,"
@@ -21,7 +22,7 @@ cursor.execute("CREATE TABLE IF NOT EXISTS Condition_Domains "
                "Domain Varchar(255))")
 
 cursor.execute("CREATE TABLE IF NOT EXISTS Measurement_Domains"
-               "(Measurement Varchar(255) PRIMARY KEY,"
+               "(Measurement_Name Varchar(255) PRIMARY KEY,"
                "Domain Varchar(255))")
 
 cursor.execute("CREATE TABLE IF NOT EXISTS Experiment_Int"
@@ -30,7 +31,7 @@ cursor.execute("CREATE TABLE IF NOT EXISTS Experiment_Int"
                "Condition_Name varchar(255),"
                "Condition_Value Int,"
                "FOREIGN KEY (Sequence) REFERENCES Sequences(Name),"
-               "FOREIGN KEY (Condition_Name) REFERENCES Condition_Domains(Condition))")
+               "FOREIGN KEY (Condition_Name) REFERENCES Condition_Domains(Condition_Name))")
 
 cursor.execute("CREATE TABLE IF NOT EXISTS Experiment_Boolean"
                "(Experiment_ID Int NOT NULL AUTO_INCREMENT,"
@@ -38,7 +39,7 @@ cursor.execute("CREATE TABLE IF NOT EXISTS Experiment_Boolean"
                "Condition_Name varchar(255),"
                "Condition_Value Bool,"
                "FOREIGN KEY (Sequence) REFERENCES Sequences(Name),"
-               "FOREIGN KEY (Condition_Name) REFERENCES Condition_Domains(Condition))")
+               "FOREIGN KEY (Condition_Name) REFERENCES Condition_Domains(Condition_Name))")
 
 cursor.execute("CREATE TABLE IF NOT EXISTS Experiment_String"
                "(Experiment_ID Int NOT NULL AUTO_INCREMENT,"
@@ -46,7 +47,7 @@ cursor.execute("CREATE TABLE IF NOT EXISTS Experiment_String"
                "Condition_Name varchar(255),"
                "Condition_Value String,"
                "FOREIGN KEY (Sequence) REFERENCES Sequences(Name),"
-               "FOREIGN KEY (Condition_Name) REFERENCES Condition_Domains(Condition))")
+               "FOREIGN KEY (Condition_Name) REFERENCES Condition_Domains(Condition_Name))")
 
 cursor.execute("CREATE TABLE IF NOT EXISTS Experiment_Float"
                "(Experiment_ID Int NOT NULL AUTO_INCREMENT,"
@@ -54,99 +55,147 @@ cursor.execute("CREATE TABLE IF NOT EXISTS Experiment_Float"
                "Condition_Name varchar(255),"
                "Condition_Value Double,"
                "FOREIGN KEY (Sequence) REFERENCES Sequences(Name),"
-               "FOREIGN KEY (Condition_Name) REFERENCES Condition_Domains(Condition))")
+               "FOREIGN KEY (Condition_Name) REFERENCES Condition_Domains(Condition_Name))")
 
 cursor.execute("CREATE TABLE IF NOT EXISTS Measurements_Int"
                "(Experiment_ID Int NOT NULL AUTO_INCREMENT,"
                "Measurement_Name varchar(255),"
                "Measurement_Value Int"
-               "FOREIGN KEY (Measurement_Name) REFERENCES Measurement_Domains(Measurement))")
+               "FOREIGN KEY (Measurement_Name) REFERENCES Measurement_Domains(Measurement_Name))")
 
 cursor.execute("CREATE TABLE IF NOT EXISTS Measurements_Boolean"
                "(Experiment_ID Int NOT NULL AUTO_INCREMENT,"
                "Measurement_Name varchar(255),"
                "Measurement_Value Bool,"
-               "FOREIGN KEY (Measurement_Name) REFERENCES Measurement_Domains(Measurement))")
+               "FOREIGN KEY (Measurement_Name) REFERENCES Measurement_Domains(Measurement_Name))")
 
 cursor.execute("CREATE TABLE IF NOT EXISTS Measurements_String"
                "(Experiment_ID Int NOT NULL AUTO_INCREMENT,"
                "Measurement_Name varchar(255),"
                "Measurement_Value varchar(255),"
-               "FOREIGN KEY (Measurement_Name) REFERENCES Measurement_Domains(Measurement))")
+               "FOREIGN KEY (Measurement_Name) REFERENCES Measurement_Domains(Measurement_Name))")
 
 cursor.execute("CREATE TABLE IF NOT EXISTS Measurements_Float"
                "(Experiment_ID Int NOT NULL AUTO_INCREMENT,"
                "Measurement_Name varchar(255),"
                "Measurement_Value Double,"
-               "FOREIGN KEY (Measurement_Name) REFERENCES Measurement_Domains(Measurement))")
+               "FOREIGN KEY (Measurement_Name) REFERENCES Measurement_Domains(Measurement_Name))")
 
 df = pd.read_csv()
 
 for column in df.columns.values:
+    df[column.str].replace('', np.nan, inplace=True)
 
     experiment = column.str.split('_', n=1)
     cursor.execute("SELECT Name "
                    "FROM Sequences "
                    "WHERE Name = " + experiment[0])
 
-    if not cursor.next():
+    sequence = cursor.fetchall()
+
+    if sequence is None:
         # Output condition missing error here
         continue
-    sequence = cursor.fetchall()
-    conditions = experiment[1].split('_')
-    condList = [conditions[index] + '_' + conditions[index+1] for index in range(len(conditions)-1)]
 
+    conditions = experiment[1].split('_')
+    condList = [conditions[index] + '_' + conditions[index + 1] for index in range(len(conditions) - 1)]
+
+    prevInsert = False
+    initCond = 0
+    initValue = 0
     for condition in condList:
         expr = condition.split('_')
         cursor.execute("SELECT Condition, Domain "
-                                "FROM Condition_Domains "
-                                "WHERE Condition = " + expr[0])
-        if not cursor.next():
-            continue
+                       "FROM Condition_Domains "
+                       "WHERE Condition_Name = " + expr[0])
+
         condRS = cursor.fetchall()
-        exp = False
-        for row in df.index.values:
-            cursor.execute("SELECT Measurement "
-                            "FROM Measurement_Domains "
-                            "WHERE Measurement = " + row.str)
-            if not cursor.next():
-                # Output measurement missing error
-                continue
-            cell = df.loc[row.str, column.str]
-            measRS = cursor.fetchall()
-            for c in condRS:
-                if not exp:
-                    cursor.execute("INSERT INTO Experiment_" + c[1] +
-                                   " (Sequence, Condition_Name, Condition_Value) VALUES "
-                                   "(" + experiment[0] + ", " + condRS[0] + ", " + cell + ')')
-                    exp = True
-                else:
-                    cursor.execute("SELECT Experiment_ID"
-                                          "FROM Experiment_" + c[1] +
-                                          " WHERE Sequence = " + experiment[0]
-                                            + " AND Condition_Name = " + condRS[0]
-                                            + " AND Condition_Value = " + cell)
-                    expIDs = cursor.fetchall()
-                    expid = 0
-                    for result in expIDs:
-                        expid = result[0]
-                    cursor.execute("INSERT INTO Experiment_" + c[1] +
-                                   " VALUES (" + expid + ", " + experiment[0] + ", "
-                                   + condRS[0] + ", " + cell + ')')
-                for r in measRS:
-                    cursor.execute("SELECT Experiment_ID"
-                                   "FROM Experiment_" + c[1] +
-                                   " WHERE Sequence = " + experiment[0]
-                                   + " AND Condition_Name = " + condRS[0]
-                                   + " AND Condition_Value = " + cell)
-                    expIDs = cursor.fetchall()
-                    expid = 0
-                    for result in expIDs:
-                        expid = result[0]
+        if condRS is None:
+            continue
 
+        for c in condRS:
+            domain = c[1]
+            value = expr[1]
+            if domain.lower() == "float":
+                domain = "Float"
+                value = float(expr[1])
+            elif domain.lower() == "boolean":
+                domain = "Boolean"
+                if expr[1].lower() in ['t', '1']:
+                    value = True
+                elif expr[1].lower() in ['f', '0']:
+                    value = False
+            elif domain.lower() == "int":
+                domain = "Int"
+                value = int(expr[1])
+            elif domain.lower() == "string":
+                domain = "String"
+                value = expr[1]
             else:
-                cursor.execute("SELECT Experiment_ID "
-                               "FROM ")
-
-
-cursor.execute("INSERT IGNORE INTO Sequences VALUES() ON DUPLICATE KEY UPDATE Name = Name)")
+                continue
+            if not prevInsert:
+                cursor.execute("INSERT INTO Experiment_" + domain +
+                               " (Sequence, Condition_Name, Condition_Value) VALUES "
+                               "(" + experiment[0] + ", " + c[0] + ", " + value + ')')
+                initCond = c[0]
+                initValue = value
+                mydb.commit()
+                prevInsert = True
+            else:
+                cursor.execute("SELECT Experiment_ID"
+                               "FROM Experiment_" + domain +
+                               " WHERE Sequence = " + experiment[0]
+                               + " AND Condition_Name = " + initCond
+                               + " AND Condition_Value = " + initValue)
+                expIDs = cursor.fetchall()
+                iD = 0
+                for result in expIDs:
+                    iD = result[0]
+                cursor.execute("INSERT INTO Experiment_" + domain +
+                               " VALUES (" + iD + ", " + experiment[0] + ", "
+                               + c[0] + ", " + value + ')')
+                mydb.commit()
+            for row in df.index.values:
+                cursor.execute("SELECT Measurement_Name, Domain "
+                               "FROM Measurement_Domains "
+                               "WHERE Measurement = " + row.str)
+                measRS = cursor.fetchall()
+                if measRS is None:
+                    # Output measurement missing error
+                    continue
+                cell = df.loc[row.str, column.str]
+                for r in measRS:
+                    rDomain = r[1]
+                    rValue = cell
+                    if rDomain.lower() == "float":
+                        rDomain = "Float"
+                        rValue = float(cell)
+                    elif rDomain.lower() == "boolean":
+                        rDomain = "Boolean"
+                        if expr[1].lower() in ['t', '1']:
+                            rValue = True
+                        elif expr[1].lower() in ['f', '0']:
+                            rValue = False
+                    elif rDomain.lower() == "int":
+                        rDomain = "Int"
+                        rValue = int(cell)
+                    elif rDomain.lower() == "string":
+                        domain = "String"
+                        rValue = str(cell)
+                    else:
+                        continue
+                    cursor.execute("SELECT Experiment_ID"
+                                   "FROM Experiment_" + domain +
+                                   " WHERE Sequence = " + experiment[0]
+                                   + " AND Condition_Name = " + c[0]
+                                   + " AND Condition_Value = " + value)
+                    expIDs = cursor.fetchall()
+                    iD = 0
+                    if expIDs is None:
+                        continue
+                    for result in expIDs:
+                        iD = result[0]
+                    cursor.execute("INSERT INTO Measurement_" + rDomain +
+                                   " VALUES (" + iD + ", " + r[0] + ", "
+                                   + rValue + ')')
+                    mydb.commit()
